81ab18412e2d4591e1b1a8147ab0abb89c9c3265
Noah Pederson <noah@packetlost.dev>
2019-10-12 12:54:52 -0500

    Removes unused struct

--- worker.go
+++ worker.go
@@ -7,9 +7,3 @@ type Worker interface {
 	DoWork(context.Context, *Job) error
 	ID() string
 }
-
-//WorkerConfig configures a worker
-type WorkerConfig struct {
-	//PollingRate is the number of milliseconds to sleep after checking the queue for a job
-	PollingRate uint8
-}
